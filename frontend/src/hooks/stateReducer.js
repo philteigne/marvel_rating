1369d9cca77875508b2338720a2badb84cff7555
Phil Teigne <philippe.teigne@gmail.com>
2024-07-31 14:57:41 -0400

    Created useReducer skeleton

---
+++ frontend/src/hooks/stateReducer.js
@@ -0,0 +1,52 @@
+import { useReducer, useEffect } from 'react';
+
+export const API_CALL_URL = "http://localhost:8080/api/"
+
+// Basic App State
+const INITIAL_STATE = {
+  userID: 1,
+
+  appView: "Home", // Home, Rankings, Rate, User
+}
+
+// Dispatch Actions
+export const ACTIONS = {
+  
+}
+
+export function reducer(state, action) {
+  switch (action.type) {
+    // case ACTIONS.GET_INGREDIENTS_USER:
+    //   return {
+    //     ...state,
+    //     ingredientList: action.payload,
+    //   }
+      
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+const useApplicationData = () => {
+
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  
+  // AI CHATBOT
+  // useEffect(() => {
+  //   if (state.chatQuery) {
+  //     handleAIChatRequest(state.chatQuery.message, state.chatSettings.chatVoice);
+  //   }
+  // }, [state.chatQuery, handleAIChatRequest]);
+
+
+  // calling useApplicationData function return these functions that changes states
+  return {
+    state,
+    dispatch
+  };
+}
+
+export default useApplicationData;
