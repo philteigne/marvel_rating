a11167a2b4962b7a057c5f46b5fe1f372ff42919
Phil Teigne <philippe.teigne@gmail.com>
2024-07-31 19:45:16 -0400

    imported application context to display selected movie

--- frontend/src/components/MovieDetails.jsx
+++ frontend/src/components/MovieDetails.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { useContext } from "react";
 import logo from '../logo.svg';
 
+import { applicationContext } from "../hooks/applicationContext";
+
 const MovieDetails = () => {
+
+  const { state, dispatch } = useContext(applicationContext);
+  console.log("Movie details", state)
+
   return(
     <div>
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Movie Title</h2>
-      <p>Movie overview text but more like a paragraph it'll be pretty long</p>
+      <img src={state.movieDetails.posterLink} alt={state.movieDetails.title} />
+      <h2>{state.movieDetails.title}</h2>
+      <p>{state.movieDetails.overview}</p>
     </div>
   );
 }
