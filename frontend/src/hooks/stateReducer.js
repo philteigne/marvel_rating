87b179df98930755d68f50a4226d2ba189e2793a
Phil Teigne <philippe.teigne@gmail.com>
2024-07-31 19:45:55 -0400

    added placeholder values to initial state and added handler for movieDetails

--- frontend/src/hooks/stateReducer.js
+++ frontend/src/hooks/stateReducer.js
@@ -6,12 +6,39 @@ export const API_CALL_URL = "http://localhost:8080/api/"
 const INITIAL_STATE = {
   userID: 1,
 
+  // App State
   appView: "Home", // Home, Rankings, Rate, User
+
+  // Ranking State
+
+  // Rate State
+  unratedMoviesList: [
+    {
+      id: 1,
+      title: "Thor",
+      posterLink: "https://image.tmdb.org/t/p/w500/pIkRyD18kl4FhoCNQuWxWu5cBLM.jpg",
+      overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat, metus ornare hendrerit mattis, metus nisl mattis nisl, interdum blandit ligula velit ut dui."
+    },
+    {
+      id: 2,
+      title: "Shang-Chi",
+      posterLink: "https://image.tmdb.org/t/p/w500/1BIoJGKbXjdFDAqUEiA2VHqkK1Z.jpg",
+      overview: "Nullam ultricies, ipsum in luctus scelerisque, nibh nibh consequat metus, quis semper urna elit quis libero."
+    },
+    {
+      id: 3,
+      title: "Black Panther Wakanda Forever",
+      posterLink: "https://image.tmdb.org/t/p/w500/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
+      overview: "Praesent risus metus, tempor non dictum non, convallis non justo. Aliquam massa ligula, tristique ac ornare ac, dictum non tortor."
+    }
+  ],
+  movieDetails: {},
 }
 
 // Dispatch Actions
 export const ACTIONS = {
-  SET_ROUTE: "SET_ROUTE"
+  SET_ROUTE: "SET_ROUTE",
+  SET_MOVIE_DETAILS: "SET_MOVIE_DETAILS",
 }
 
 export function reducer(state, action) {
@@ -21,11 +48,12 @@ export function reducer(state, action) {
         ...state,
         appView: action.payload,
       }
-    // case ACTIONS.GET_INGREDIENTS_USER:
-    //   return {
-    //     ...state,
-    //     ingredientList: action.payload,
-    //   }
+    case ACTIONS.SET_MOVIE_DETAILS:
+      return {
+        ...state,
+        movieDetails: action.payload,
+      }
+
       
     default:
       throw new Error(
