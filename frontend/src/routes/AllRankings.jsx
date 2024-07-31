db498d3a220ea2f07e5ec949dcca566a8f988b34
Phil Teigne <philippe.teigne@gmail.com>
2024-07-31 13:57:27 -0400

    Created skeleton for AllRankings Route

--- frontend/src/routes/AllRankings.jsx
+++ frontend/src/routes/AllRankings.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
 const AllRankings = () => {
-  
+  return(
+    <React.Fragment>
+      <div>
+        <button>Fight</button>
+        <button>Comedy</button>
+        <button>Wow</button>
+        <button>Future Storyline</button>
+        <button>Villain</button>
+        <button>Story</button>
+        <button>Total Rating</button>
+      </div>
+      <ul>
+        <li>
+          <h2>Thor</h2>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>6</p>
+          <p>1</p>
+          <p>3</p>
+          <p>4</p>
+        </li>
+      </ul>
+    </React.Fragment>
+  );
 }
+
+export default AllRankings;
