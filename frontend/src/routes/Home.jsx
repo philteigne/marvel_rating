47a3444ab633163548a489c44b2525cda3f4234b
Phil Teigne <philippe.teigne@gmail.com>
2024-07-31 19:46:11 -0400

    added conditional rendering to MovieDetails component

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-
+import { useContext } from "react";
 
 import MovieDetails from "../components/MovieDetails";
+import { applicationContext } from "../hooks/applicationContext";
 
 const Home = () => {
+
+  const { state, dispatch } = useContext(applicationContext);
+
   return (
     <div>
         <h1>
           Top Movie
         </h1>
-        <MovieDetails />
+        {state.movieDetails.title && <MovieDetails />}
       </div>
   );
 }
