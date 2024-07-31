8d518fb1540ad47016836d5a94d90364f1e245af
Phil Teigne <philippe.teigne@gmail.com>
2024-07-31 14:57:12 -0400

    import Reducer and Context, provide context to react components, and update Rankings import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,36 @@
 import './App.css';
 
 import Home from './routes/Home';
-import AllRankings from './routes/AllRankings';
+import Rankings from './routes/Rankings';
 import Rate from './routes/Rate';
 import User from './routes/User';
 
+import useApplicationData from './hooks/stateReducer';
+import { applicationContext } from './hooks/applicationContext';
+
 function App() {
 
-  const state = {
-    currentView: "User",
-  }
+  const { state, dispatch } = useApplicationData();
+
+
 
   return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>All Rankings</li>
-          <li>Rate</li>
-          <li>User Settings</li>
-        </ul>
-      </nav>
-      {state.currentView === "Home" && <Home />}
-      {state.currentView === "AllRankings" && <AllRankings />}
-      {state.currentView === "Rate" && <Rate />}
-      {state.currentView === "User" && <User />}
-    </div>
+    <applicationContext.Provider value={{state, dispatch}}>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>All Rankings</li>
+            <li>Rate</li>
+            <li>User Settings</li>
+          </ul>
+        </nav>
+        {state.currentView === "Home" && <Home />}
+        {state.currentView === "Rankings" && <Rankings />}
+        {state.currentView === "Rate" && <Rate />}
+        {state.currentView === "User" && <User />}
+      </div>
+    </applicationContext.Provider>
   );
 }
 
