37f2a807dc5f62a8cd47e3820ed8ea9a94069734
Phil Teigne <philippe.teigne@gmail.com>
2024-08-01 21:26:40 -0400

    added rateMovie to state to track user ratings and added Post handler for rateMovie form submissions

--- frontend/src/hooks/stateReducer.js
+++ frontend/src/hooks/stateReducer.js
@@ -15,6 +15,8 @@ const INITIAL_STATE = {
   // MyRatings State
   unratedMoviesList: [],
   selectedMovieRate: {},
+  rateMovie: {},
+  categoriesList: [],
 }
 
 // Dispatch Actions
@@ -23,6 +25,8 @@ export const ACTIONS = {
   SET_SELECTED_MOVIE_RATE: "SET_SELECTED_MOVIE_RATE",
   SET_CAROUSEL: "SET_CAROUSEL",
   SET_UNRATED_MOVIES: "SET_UNRATED_MOVIES",
+  SET_CATEGORIES_LIST: "SET_CATEGORIES_LIST",
+  SET_RATE_MOVIE: "SET_RATE_MOVIE",
 }
 
 export function reducer(state, action) {
@@ -46,6 +50,16 @@ export function reducer(state, action) {
       return {
         ...state,
         unratedMoviesList: action.payload
+      }
+    case ACTIONS.SET_CATEGORIES_LIST:
+      return {
+        ...state,
+        categoriesList: action.payload
+      }
+    case ACTIONS.SET_RATE_MOVIE:
+      return {
+        ...state,
+        rateMovie: action.payload
       }
       
     default:
@@ -72,7 +86,7 @@ const useApplicationData = () => {
       .catch(err => console.log(err.message))
   }, [])
   
-
+  // GET unratedMoviesList
   useEffect(() => {
     fetch(`${API_CALL_URL}movies/no-ratings?user_id=${state.userID}`, {
       headers: {
@@ -82,6 +96,31 @@ const useApplicationData = () => {
     .then((res) => res.json())
     .then((data) => dispatch({ type: ACTIONS.SET_UNRATED_MOVIES, payload: data}))
   }, [state.userID])
+
+  // GET categoriesList
+  useEffect(() => {
+    fetch(`${API_CALL_URL}categories`, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then((res) => res.json())
+    .then((data) => dispatch({ type: ACTIONS.SET_CATEGORIES_LIST, payload: data}))
+  }, [])
+
+  // POST rateMovie
+  useEffect(() => {
+    fetch(`${API_CALL_URL}ratings`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(state.rateMovie)
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+  }, [state.rateMovie]) 
 
   // AI CHATBOT
   // useEffect(() => {
