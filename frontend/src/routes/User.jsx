2e00f2a488ca7c82badb0d1b5615fc95bd5bad1d
Phil Teigne <philippe.teigne@gmail.com>
2024-08-02 07:33:56 -0400

    Created and imported Login component

--- frontend/src/routes/User.jsx
+++ frontend/src/routes/User.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Login from "../components/Login";
 
 const User = () => {
   return(
     <React.Fragment>
-      
+      <Login />
     </React.Fragment>
   )
 }
