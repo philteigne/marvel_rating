728e643c490d87ec8e7cd2895e74b2fe85530513
Phil Teigne <philippe.teigne@gmail.com>
2024-08-02 15:17:00 -0400

    added conditional variables for empty state and added class naming for css

--- frontend/src/routes/Rankings.jsx
+++ frontend/src/routes/Rankings.jsx
@@ -1,3 +1,6 @@
+import '../App.css'
+import '../styles/Rankings.css'
+
 import React from "react";
 import { useContext } from "react";
 import { applicationContext } from "../hooks/applicationContext";
@@ -9,26 +12,43 @@ const Rankings = () => {
   return(
     <React.Fragment>
       <div>
-        {/* only display categories list if categoriesList is populated */}
-        {state.categoriesList.map((category) => {
-          return(
-            <button key={category.id} onClick={() => dispatch({ type: "SET_SELECTED_CATEGORY", payload: category.name })}>
-              {category.name}
-            </button>
-          )
-        })}
-        <button onClick={() => dispatch({ type: "SET_SELECTED_CATEGORY", payload: null })}>
-          Total Rating
-        </button>
+        <div className='header-controls'>
+          <h1>Movie Rankings By Category</h1>
+          <div className="view-controls">
+            <button>Cat</button>
+            <button>List</button>
+          </div>
+        </div>
+        <ul className='category-list'>
+          {state.categoriesList.length > 0 &&
+            state.categoriesList.map((category) => {
+              return(
+                <li key={category.id} onClick={() => dispatch({ type: "SET_SELECTED_CATEGORY", payload: category.name })} className='category-item'>
+                  {category.name}
+                </li>
+              )
+            })
+          }
+          <li onClick={() => dispatch({ type: "SET_SELECTED_CATEGORY", payload: null })} className='category-item'>
+            Total Rating
+          </li>
+        </ul>
       </div>
-      <ul>
-        {state.rankedMoviesList.map((movie) => {
-          return(
-            <li key={movie.id}>
-              {movie.title}: {movie.average_rating}
-            </li>
-          )
-        })}
+      <ul className='movie-list'>
+        {state.rankedMoviesList.length > 0 &&
+          state.rankedMoviesList.map((movie, index) => {
+            return(
+              <li key={movie.id} className='movie-item'>
+                <h2 className='movie-ranking'>{index + 1}.</h2>
+                <div className='movie-container'>
+                  <img src={movie.poster_url} className='movie-poster'></img>
+                  <p className='movie-title'>{movie.title}</p>
+                  <p className='movie-rating'>{state.selectedCategory || "Total"} Rating: {movie.average_rating}</p>
+                </div>
+              </li>
+            )
+          })
+        }
       </ul>
     </React.Fragment>
   );
