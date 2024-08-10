e68b7209f0d7fc6fb2056d97bf1945f44a178dde
Phil Teigne <philippe.teigne@gmail.com>
2024-08-10 15:13:20 -0400

    Added seciton to display average reating

--- frontend/src/components/MovieDetails.jsx
+++ frontend/src/components/MovieDetails.jsx
@@ -14,11 +14,19 @@ const MovieDetails = () => {
 
   return(
     <div className='movie-display'>
-      <img src={highestMovie.poster_url} alt={highestMovie.title} />
+      <img src={highestMovie.poster_url} alt={highestMovie.title} className='movie-poster'/>
       <div className='movie-description'>
-        <h2 className='movie-award'>Top Rated Movie</h2>
-        <h3 className='movie-title'>{highestMovie.title}</h3>
-        <p className='movie-overview'>{highestMovie.overview}</p>
+        <div>
+          <h2 className='movie-award'>Top Rated Movie</h2>
+          <h3>{highestMovie.title}</h3>
+          <p className='movie-overview'>{highestMovie.overview}</p>
+        </div>
+        <div>
+          <h3>Average Friend Rating:</h3>
+          <div>
+            {highestMovie.average_rating}
+          </div>
+        </div>
       </div>
     </div>
   );
