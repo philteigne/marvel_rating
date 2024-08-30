28ba2bab0936927c74d31dd2691b5bf7f2b7b04c
Phil Teigne <philippe.teigne@gmail.com>
2024-08-29 23:20:18 -0400

    added welcome splash

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,5 +1,4 @@
 import '../App.css'
-import '../styles/Home.css'
 
 import React from "react";
 import { useContext } from "react";
@@ -12,11 +11,31 @@ const Home = () => {
   const { state, dispatch } = useContext(applicationContext);
 
   return (
-    <div className='movie-showcase'>
-      <h1 className='page-header'>
-        Your Friends' Top Picks
-      </h1>
-      {state.carouselMovies.highest_rated_movie && <MovieDetails />}
+    <div>
+      {state.authToken &&
+        <div className='movie-showcase'>
+          <h1>
+            Friends' Top Picks
+          </h1>
+          {state.carouselMovies.highest_rated_movie && <MovieDetails />}
+        </div>
+      }
+      {!state.authToken &&
+        <div className='welcome-container'>
+          <h1 className='page-header'>
+            Welcome To TasteBuds
+          </h1>
+          <p>
+            The ultimate destination for movie lovers who enjoy sharing their cinematic adventures with friends!
+            Here at TasteBuds, you can rate movies, discover new films, and see what your friends think about the latest releases. Dive into a world where your taste in movies connects you with like-minded friends, creating a vibrant community of cinephiles.
+            Whether you're raving about a blockbuster hit or dissecting an indie gem, TasteBuds is the perfect place to share your thoughts and discover your next favorite film together. Happy rating!
+          </p>
+          <button onClick={() => dispatch({type: "SET_ROUTE", payload: "User"})}>
+            Get Started
+          </button>
+        </div>
+      }
+
     </div>
   );
 }
