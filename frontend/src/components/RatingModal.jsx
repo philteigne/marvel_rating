c97afbb8ea22461997596a98e6549563af3746cf
Phil Teigne <philippe.teigne@gmail.com>
2024-08-30 18:34:53 -0400

    added modal styling

--- frontend/src/components/RatingModal.jsx
+++ frontend/src/components/RatingModal.jsx
@@ -8,6 +8,7 @@ const RatingModal = () => {
   const { state, dispatch } = useContext(applicationContext);
   const [movieRating, setMovieRating] = useState({})
   const [ratingTotal, setRatingTotal] = useState(0)
+  const [inputValue, setInputValue] = useState('')
 
   const findRatingAverage = (movieObj) => {
     const keys = Object.keys(movieObj);
@@ -60,54 +61,58 @@ const RatingModal = () => {
           dispatch({type: "CLEAR_SELECTED_MOVIE_RATE"})
         }}>&times;</span>
         <div className="modal-content">
-          <img src={state.selectedMovieRate.poster_url} className="modal-movie-poster"/>
-
           <div className="modal-info">
+            <img src={state.selectedMovieRate.poster_url} className="modal-movie-poster"/>
             <div className="modal-movie-details">
-              <h3>{state.selectedMovieRate.title}</h3>
+              <h3 className="modal-header">{state.selectedMovieRate.title}</h3>
               <p className='movie-overview'>{state.selectedMovieRate.overview}</p>
+              <h3 className="modal-header">Average Rating</h3>
+              <p>{state.selectedMovieRate.average_rating || "No Ratings Yet"}</p>
+
             </div>
-            <form onSubmit={handleSubmit} className="modal-rating-form">
-              <div className="modal-rating-input-container">
-                {state.categoriesList.map((category) => {
-                  // if movie has rating for this category, set the value of the input to match the rating
-                  let inputValue = 0;
-                  if (state.selectedMovieRate.ratings.length > 0) {
-                    for(let item of state.selectedMovieRate.ratings) {
-                      if (item.category_id === category.id) {
-                        inputValue = item.rating;
-                        break;
-                      }
+            
+          </div>
+          <form onSubmit={handleSubmit} className="modal-rating-form">
+            <h3 className="modal-header">Your Ratings</h3>
+            <div className="modal-rating-input-container">
+              {state.categoriesList.map((category) => {
+                // if movie has rating for this category, set the value of the input to match the rating
+                let inputValue = 0;
+                if (state.selectedMovieRate.ratings.length > 0) {
+                  for(let item of state.selectedMovieRate.ratings) {
+                    if (item.category_id === category.id) {
+                      inputValue = item.rating;
+                      break;
                     }
                   }
-                  
-                  return (
-                    <div className="modal-rating-input">
-                      <label key={category.id} className="modal-rating-label">{category.name}</label>
-                      <input type="number" min="1" max="10" step="1" defaultValue={inputValue} onChange={(e) => { 
-                        setMovieRating({
-                          ...movieRating,
-                          [category.id]: {
-                            id: category.id,
-                            rating: e.target.value
-                          }
-                        })
-                      }}
-                      className="modal-rating-input-number"></input>
-                    </div>
-                  )
-                })}
+                }
+                
+                return (
+                  <div className="modal-rating-input">
+                    <label key={category.id} className="modal-rating-label">{category.name}</label>
+                    <input type="number" min="1" max="10" step="1" defaultValue={inputValue} onChange={(e) => { 
+                      setMovieRating({
+                        ...movieRating,
+                        [category.id]: {
+                          id: category.id,
+                          rating: e.target.value
+                        }
+                      })
+                    }}
+                    className="modal-rating-input-number"></input>
+                  </div>
+                )
+              })}
+            </div>
+            <div className="modal-rating-summary">
+              <div className="modal-rating-input">
+                <label className="modal-rating-label">Total</label>
+                <input type="number" value={ratingTotal} readonly className="modal-rating-input-number"></input>
               </div>
-              <div className="modal-rating-summary">
-                <div className="modal-rating-input">
-                  <label className="modal-rating-label">Total</label>
-                  <input type="number" value={ratingTotal} readonly className="modal-rating-input-number"></input>
-                </div>
-                <button type="submit" className="modal-submit-button" onClick={() => handleSubmit}
-                >Submit</button>
-              </div>
-            </form>
-          </div>
+              <button type="submit" className="modal-submit-button" onClick={() => handleSubmit}
+              >Submit</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
