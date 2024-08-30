77a3e39a2761a72df9cc3590ac9b3ba173205f0a
Phil Teigne <philippe.teigne@gmail.com>
2024-08-30 16:20:19 -0400

    Updated styling to Rankings view

--- frontend/src/routes/Rankings.jsx
+++ frontend/src/routes/Rankings.jsx
@@ -11,25 +11,19 @@ const Rankings = () => {
   return(
     <div className='ranking-container'>
       <div>
-        <div className='header-controls'>
-          <h1>Movie Rankings By Category</h1>
-          <div className="view-controls">
-            <button>Cat</button>
-            <button>List</button>
-          </div>
-        </div>
+        <h1>Rankings By Category</h1>
         <ul className='category-list'>
           {state.categoriesList.length > 0 &&
             state.categoriesList.map((category) => {
               return(
                 <li key={category.id} onClick={() => dispatch({ type: "SET_SELECTED_CATEGORY", payload: category.name })} className='category-item'>
-                  {category.name}
+                  <h3>{category.name}</h3>
                 </li>
               )
             })
           }
           <li onClick={() => dispatch({ type: "SET_SELECTED_CATEGORY", payload: null })} className='category-item'>
-            Total Rating
+            <h3>Overall</h3>
           </li>
         </ul>
       </div>
@@ -38,10 +32,10 @@ const Rankings = () => {
           state.rankedMoviesList.map((movie, index) => {
             return(
               <li key={movie.id} className='movie-ranking-item'>
-                <h2 className='movie-ranking'>{index + 1}.</h2>
+                <h4 className='movie-ranking'>{index + 1}</h4>
                 <div className='movie-ranking-container'>
                   <img src={movie.poster_url} className='movie-ranking-poster'></img>
-                  <p className='movie-ranking-title'>{movie.title}</p>
+                  <h4 className='movie-ranking-title'>{movie.title}</h4>
                   <p className='movie-ranking-rating'>{state.selectedCategory || "Total"} Rating: {movie.average_rating}</p>
                 </div>
               </li>
